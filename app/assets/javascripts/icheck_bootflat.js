bb92af376c9055bb6e176547b35f48a9fee607c4
m.taira <taira.masaru@gmail.com>
2014-10-04 11:15:39 +0900

    bootflat追加

---
+++ app/assets/javascripts/icheck_bootflat.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('.checkbox input').iCheck({
+    checkboxClass: 'icheckbox_flat',
+    cursor: true
+  });
+
+  $('.radio input').iCheck({
+    radioClass: 'iradio_flat',
+    cursor: true
+  });
+});
+
